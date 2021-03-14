4641e7fd011479e92a1d9e625af4680f85cdc969
gsoaresp92@gmail.com <gsoaresp92@gmail.com>
2021-03-14 15:44:12 -0300

    added DI container and http server

---
+++ index.js
@@ -0,0 +1,15 @@
+const startApplication = async () => {
+
+    const container = require('./src/container');
+
+    const app = container.resolve('application');
+
+    app
+        .start()
+        .catch((error) => {
+            console.log(error)
+            process.exit();
+        });
+};
+
+startApplication();
